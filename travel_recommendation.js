9bcb1bf31d08aa92a17bdef88bad301e998c4cd2
Ilyas Bambrik <ilyas.off.script@gmail.com>
2024-05-29 21:04:48 +0200

    version 2- with all features working

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -5,10 +5,12 @@ document.getElementById("clear-btn").addEventListener("click",()=>{
     resulsection.style.display="none"
     resulsection.innerHTML=""
 })
+let result=[]
 document.getElementById('search-btn')
 .addEventListener("click",()=>{
     resulsection.innerHTML=""
     let searchkeyword=document.getElementById("search-input").value.toLowerCase()
+    let query=searchkeyword
     searchkeyword = ({"beach":"beaches","temple":"temples","country":"countries"})[searchkeyword]
     fetch("./travel_recommendation.json")
     .then(data=>data.json())
@@ -18,13 +20,13 @@ document.getElementById('search-btn')
             return key===searchkeyword}
 
         )!=-1){
+            result=[]
             const res=jsdata[searchkeyword]
-            let result=res
+            result=res
             if(res[0].cities){
                 result=[]
                 res.map(({cities})=>result=result.concat(cities))
             }
-            console.log(JSON.stringify(result))
                 result.map(destination=>{
                 const {imageUrl,name,description}=destination
                 const div=document.createElement("div")
@@ -40,9 +42,35 @@ document.getElementById('search-btn')
             </div>                `
                 resulsection.appendChild(div)
             })
+
             resulsection.style.display='block'
 
         }
+        if(!result.length){
+            console.log('###########')
+            let res1=[]
+            jsdata.temples.concat(jsdata.beaches).concat(jsdata.countries).map(dest=>{
+                if(!dest.cities)res1.push(dest)
+                else res1=res1.concat(dest.cities)
+            })
+            res1.map(destination=>{
+                const {imageUrl,name,description}=destination
+                if(!description.toLowerCase().includes(query) && !name.toLowerCase().includes(query)) return
+                const div=document.createElement("div")
+                div.innerHTML=`
+                <div class="recommendation">                            
+                <img class="recommendation-img" src="./public/destinations/${imageUrl}">
+                <div class="recommendation-description">
+                    <h6>${name}</h6>
+                    <p>${description}</p>
+                    <button class="visit-btn">Visit</button>
+                </div>
+                
+            </div>                `
+                resulsection.appendChild(div)
+            })
+        }
+
         
     })
     document.getElementById("result-section").style.display="block"
